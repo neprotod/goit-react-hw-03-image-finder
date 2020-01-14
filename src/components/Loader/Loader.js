49062ab2350801dd792d5d9669648183bfdc019a
Alexander Podlesnyi <gwebdzen@gmail.com>
2020-01-14 13:42:46 +0200

    Add loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <Loader
+	     type="Grid"
+	     color="#00BFFF"
+	     height={80}
+	     width={80}
+	     timeout={3000} //3 secs
+
+	  />
+  );
+};
+
+export default Loader;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  id: '14899095-5998dc2cbeb0e1f3fce4bf75e',
+};
