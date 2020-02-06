3cafa45e8f3662b67b72a594fe6ac1fe6c9f5c33
Alexander Podlesnyi <gwebdzen@gmail.com>
2020-02-06 15:21:09 +0200

    Updates

---
+++ precache-manifest.efedd9eef217d110c8cfa773cc69d58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ffd6f795bed2b6d8e210d00face736",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "628c58ea2ffbd9f35293",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.d1320125.chunk.css"
+  },
+  {
+    "revision": "6fbf5dba18b38a35801a",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.aa814eeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.aa814eeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "628c58ea2ffbd9f35293",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.b30ffa56.chunk.js"
+  },
+  {
+    "revision": "a1a6907170055b101ffa",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.fa670728.js"
+  }
+]);
